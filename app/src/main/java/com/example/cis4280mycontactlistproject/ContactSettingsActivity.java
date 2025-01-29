bafa6db933b7c4d4082ba9c2d1f10f7420219013
anhphan <anhnguyenphan03@gmail.com>
2025-01-28 21:26:06 -0500

    Toggle Button Function

--- app/src/main/java/com/example/cis4280mycontactlistproject/ContactSettingsActivity.java
+++ app/src/main/java/com/example/cis4280mycontactlistproject/ContactSettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.cis4280mycontactlistproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,44 @@ public class ContactSettingsActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+
+    }
+
+    private void initListButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initMapButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonMap);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactSettingsActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/cis4280mycontactlistproject/MainActivity.java
+++ app/src/main/java/com/example/cis4280mycontactlistproject/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.cis4280mycontactlistproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         initListButton();
         initMapButton();
         initSettingsButton();
+        initToggleButton();
+        setForEditing(false);
+
     }
 
     private void initListButton() {
@@ -61,4 +68,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initToggleButton() {
+        final ToggleButton editToggle = (ToggleButton)findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setForEditing(editToggle.isChecked());
+            }
+        });
+    }
+
+    private void setForEditing(boolean enabled) {
+        EditText editName = findViewById(R.id.editName);
+        EditText editAddress = findViewById(R.id.editAddress);
+        EditText editCity = findViewById(R.id.editCity);
+        EditText editState = findViewById(R.id.editState);
+        EditText editZipcode = findViewById(R.id.editZipcode);
+        EditText editPhone = findViewById(R.id.editHome);
+        EditText editCell = findViewById(R.id.editCell);
+        EditText editEmail = findViewById(R.id.editEmail);
+        Button buttonChange = findViewById(R.id.btnBirthday);
+        Button buttonSave = findViewById(R.id.buttonSave);
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipcode.setEnabled(enabled);
+        editPhone.setEnabled(enabled);
+        editCell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+        buttonChange.setEnabled(enabled);
+        buttonSave.setEnabled(enabled);
+
+        if (enabled) {
+            editName.requestFocus();
+        }
+    }
+
 }
