4bb1b537cceba5c24f4f111c3094075f0dbb103d
anhphan <anhnguyenphan03@gmail.com>
2025-02-26 15:00:06 -0500

    Geocoding: Get Coordinates from an Address

--- app/src/main/java/com/example/cis4280mycontactlistproject/ContactMapActivity.java
+++ app/src/main/java/com/example/cis4280mycontactlistproject/ContactMapActivity.java
@@ -1,15 +1,23 @@
 package com.example.cis4280mycontactlistproject;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.IOException;
+import java.util.List;
 
 public class ContactMapActivity extends AppCompatActivity {
 
@@ -27,6 +35,7 @@ public class ContactMapActivity extends AppCompatActivity {
         initListButton();
         initMapButton();
         initSettingsButton();
+        initGetLocationButton();
 
     }
 
@@ -59,6 +68,38 @@ public class ContactMapActivity extends AppCompatActivity {
                 Intent intent = new Intent(ContactMapActivity.this, ContactSettingsActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
+            }
+        });
+    }
+    private void initGetLocationButton() {
+        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editAddress = (EditText) findViewById(R.id.addressMapET);
+                EditText editCity = (EditText) findViewById(R.id.cityMapET);
+                EditText editState = (EditText) findViewById(R.id.stateMapET);
+                EditText editZipcode = (EditText) findViewById(R.id.zipcodeMapET);
+
+                String address = editAddress.getText().toString() + ", " +
+                        editCity.getText().toString() + ", " +
+                        editState.getText().toString() + ", " +
+                        editZipcode.getText().toString();
+
+                List<Address> addresses = null;
+                Geocoder geo = new Geocoder(ContactMapActivity.this);
+                try {
+                    addresses = geo.getFromLocationName(address, 1);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+
+                txtLatitude.setText(String.valueOf(addresses.get(0).getLatitude()));
+                txtLongitude.setText(String.valueOf(addresses.get(0).getLongitude()));
             }
         });
     }
